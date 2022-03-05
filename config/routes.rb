606db9eac26f55707883b7c48594d64842b29e80
EC2 Default User <ec2-user@ip-172-31-10-93.ap-northeast-1.compute.internal>
2022-03-05 17:08:06 +0000

    finish 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,29 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
+
+- resources :todos
+
+- resources :goals
+
++ resources :goals do
+
++   resources :todos do
+
++     member do
+
++       get "sort"
+
++     end
+
++   end
+
++ end
+
+
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+
   root to: "home#index"
+
 end
